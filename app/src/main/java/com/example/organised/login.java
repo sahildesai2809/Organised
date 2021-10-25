8e086272c49d6474cefbd33360b66bf233e88fc3
Sahil <sahild2809@gmail.com>
2021-10-25 16:54:02 +0530

    add login

--- app/src/main/java/com/example/organised/login.java
+++ app/src/main/java/com/example/organised/login.java
@@ -5,18 +5,62 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class login extends AppCompatActivity {
-    private TextView signup_text;
+    private TextView signup_text,login_text, signupp_text,signup_txt;
+    private EditText username,password;
+
+    DBHelper DB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         signup_text = findViewById(R.id.signup_text);
+        DB = new DBHelper(this);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.pass);
+        login_text = findViewById(R.id.login_text);
+//        signup_text = findViewById(R.id.signupp_text);
+        signup_txt = findViewById(R.id.signup_txt);
 
-        signup_text.setOnClickListener(new View.OnClickListener() {
+
+
+
+        login_text.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                if(user.equals("")||pass.equals(""))
+                    Toast.makeText(login.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                else{
+                    Boolean checkuserpass = DB.checkusernamepassword(user, pass);
+                    if(checkuserpass==true){
+                        Toast.makeText(login.this, "Sign in successfull", Toast.LENGTH_SHORT).show();
+                        Intent intent  = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(login.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                    }
+                };
+
+
+            }
+        });
+
+
+//        signup_text.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                gotosignup();
+//
+//            }
+//        });
+        signup_txt.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 gotosignup();
 
@@ -25,7 +69,9 @@ public class login extends AppCompatActivity {
     }
 
     public  void gotosignup(){
-        Intent intent = new Intent(this, signup.class);
+//        Intent intent = new Intent(this, signup.class);
+//        startActivity(intent);
+        Intent intent = new Intent(this,signup.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/organised/signup.java
+++ app/src/main/java/com/example/organised/signup.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class signup extends AppCompatActivity {
 
     private TextView lgin_text;
     private TextView signup_btn;
+    private EditText username, password, confirmpassword;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,16 @@ public class signup extends AppCompatActivity {
 
         lgin_text = findViewById(R.id.lgin_text);
         signup_btn = findViewById(R.id.signup_btn);
+        DB= new DBHelper(this);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.pass);
+//        confirmpassword = findViewById(R.id.confirmpass);
+
+
+
+
+
 
         lgin_text.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -28,7 +42,32 @@ public class signup extends AppCompatActivity {
         });
         signup_btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                gotohome();
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+//                String repass = confirmpassword.getText().toString();
+                if(user.equals("")||pass.equals(""))
+                    Toast.makeText(signup.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                else{
+                    if(true){
+                        Boolean checkuser = DB.checkusername(user);
+                        if(!checkuser){
+                            Boolean insert = DB.insertData(user, pass);
+                            if(insert){
+                                Toast.makeText(signup.this, "Registered successfully", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                                startActivity(intent);
+                            }else{
+                                Toast.makeText(signup.this, "Registration failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else{
+                            Toast.makeText(signup.this, "User already exists! please sign in", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(signup.this, "Passwords not matching", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
 
             }
         });
@@ -39,8 +78,8 @@ public class signup extends AppCompatActivity {
         startActivity(intent);
 
     }
-    public  void gotohome(){
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
+//    public  void gotohome(){
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+//    }
 }
